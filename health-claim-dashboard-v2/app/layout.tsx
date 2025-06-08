c8021a6ea5114be3ce2593bb24db38d67e2517bf
Hrishabh Ayush <hrishabh.ay@gmail.com>
2025-06-07 21:54:11 -0400

    fixed bugs with .gitignore and v2 folder

--- health-claim-dashboard-v2/app/layout.tsx
+++ health-claim-dashboard-v2/app/layout.tsx
@@ -19,9 +19,14 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
+        <ThemeProvider 
+          attribute="class" 
+          defaultTheme="system" 
+          enableSystem 
+          disableTransitionOnChange
+        >
           {children}
           <Toaster />
         </ThemeProvider>
